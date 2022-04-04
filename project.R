ebd1541e11e8d11a1276bd411cfbda4f1abff725
mitja-mandic <mitja.mandic@gmail.com>
2022-04-04 14:51:53 +0200

    Images for the report

--- project.R
+++ project.R
@@ -6,7 +6,8 @@ mygroup <- which(rmultinom(1, 1, c(0.25,0.25,0.25,0.25)) == 1)
 mysample <- sample(which(y == mygroup), 180)
 X_train <- data.frame(X[mysample[1:90], ])
 X_valid <- data.frame(X[mysample[91:180], ])
-#matplot(t(X[1300:1600,]), lty=1, type="l")
+matplot(t(cbind(X,y)), lty=1, type="l",col = y,xlab = "Wavelength",ylab="",main = "Spectra")
+legend("topleft",legend=c("D","H","Ha","E"),col=y,type="l")
 
 #Check column variances to see if we need to scale variables:
 X_train %>% as.data.frame() %>% summarise_if(is.numeric, var)
@@ -17,8 +18,9 @@ X_train %>% as.data.frame() %>% summarise_if(is.numeric, var)
 #work with a correlation matrix
 train.pca <- PcaClassic(X_train, scale = T)
 train.pca.prcomp <- getPrcomp(train.pca)
-plot(train.pca$scores)
+plot(train.pca$scores, main = "Classic PCA Scores", pch=19)
 summary(train.pca)
+
 plot(train.pca$eigenvalues/sum(train.pca$eigenvalues),type="b")
 fviz_eig(train.pca.prcomp,choice="eigenvalue")
 
@@ -29,15 +31,17 @@ fviz_eig(train.pca.prcomp,choice="eigenvalue")
 
 #select 2 components
 train.pca.2 <- PcaClassic(X_train, k=2, scale=T)
-plot(train.pca.2)
+plot(train.pca.2,pch=19)
 outliers <- c(14,33,42,48,88)
 #biplot(train.pca.2, scale=0, cex=c(0.7,1))
 #plot(train.pca,6,crit.pca.distances=0.99)
 
 
 train.pca.2$scores
-par(mfrow=c(2,1))
-matplot(train.pca.2$loadings, type="l", xlab="Wavelength", ylab="Loadings", main="classical PCA loadings")
+
+matplot(train.pca.2$loadings, type="l", xlab="Wavelength", ylab="Loadings", main="Classical PCA loadings")
+legend("topleft",legend = c("v1","v2"),col=c("black","red"),lty=c(1,2))
+
 matplot(t(X_train),type="l")
 # train.pca.6 <- PcaClassic(X, k=6, scale = T)
 # plot(train.pca.6,crit.pca.distances=0.99)
@@ -45,14 +49,16 @@ par(mfrow=c(1,1))
 # matplot(train.pca.2$scores,type='l')
 # pairs(train.pca.2$scores)#, col=y)
 plot(train.pca.2$scores,col=y,asp=1, pch=19, main="Classic PCA scores")
-plot(train.pca.2,crit.pca.distances=0.99, pch=19)
+#plot(train.pca.2,crit.pca.distances=0.99, pch=19)
 
 #QUESTION 3
 
 #Robust PCA
 train.robpca.2 <- PcaHubert(X_train,k=2,scale = T)
-plot(train.robpca.2)
-matplot(train.robpca.2$loadings, type="l", xlab="Wavelength", ylab="Loadings",main="robust PCA loadings")
+plot(train.robpca.2,pch=19)
+matplot(train.robpca.2$loadings, type="l", xlab="Wavelength", ylab="Loadings",main="Robust PCA loadings")
+legend("bottomleft",legend = c("v1","v2"),col=c("black","red"),lty=c(1,2))
+
 plot(train.robpca.2$scores,asp=1, main="Robust PCA scores",pch=19)
 
 #QUESTION 4
@@ -61,14 +67,16 @@ plot(train.robpca.2$scores,asp=1, main="Robust PCA scores",pch=19)
 which_pca <- train.robpca.2
 
 X_valid.standardized <- scale(X_valid,center=which_pca$center, scale=which_pca$scale)
+Dmat_X <- diag(which_pca$scale)
 
-X_valid.scores <- X_valid.standardized %*% which_pca$loadings
+
+X_valid.scores <- as.matrix(X_valid.standardized) %*% which_pca$loadings
 X_valid.fitted <- t(t(X_valid.scores %*% t(which_pca$loadings)) + which_pca$center/which_pca$scale)
 
 X_valid.scaled <- scale(X_valid,center = F, scale = which_pca$scale)
 euclnorm <- function(y) sqrt(sum(y^2))
 
-og.distance <- apply(X_valid.scaled -X_valid.fitted, 1, euclnorm)
+og.distance <- apply(X_valid.scaled - X_valid.fitted, 1, euclnorm)
 score.distance <- sqrt(mahalanobis(X_valid.scores, center = FALSE, diag(which_pca$eigenvalues)))
 
 cutoff.sd <- which_pca$cutoff.sd
@@ -81,15 +89,17 @@ xmax <- max(sds, cutoff.sd,score.distance)
 ymax <- max(ods, cutoff.od,og.distance)
 
 
-#outl.index <- (length(ods)-3) : length(ods)
-#colors <- rep("black", length(ods))
-#colors[outl.index] <- c("red", "red", "red", "orange")
 plot(sds, ods, pch = 19, xlim = c(0, xmax + 1), ylim = c(0, ymax + 0.2),# col = colors,
      main = "Outlier plot of predicted and training data", xlab = "Score distance", ylab = "Orthogonal distance", col = "black")
 points(new.distances,col = "blue",pch=19)
 legend("topright",legend = c("Predicted values","Training data values"),col=c("blue","black"),pch=19)
 abline(h = cutoff.od, col = "red", lwd = 1.5)
 abline(v = cutoff.sd, col ="red", lwd = 1.5)
+
+Xhat <- X_valid.fitted %*% Dmat_X
+matplot(t(Xhat),lty=1,type="l",xlab = "Wavelength",ylab="",main = "Predicted spectra, k=2",ylim = c(-2,2))
+matplot(t(X_valid),type="l",lty=1, xlab = "Wavelength",ylab="", main="Observed validation data spectra",ylim = c(-2,2))
+
 
 
 #QUESTION 5
@@ -102,5 +112,3 @@ shapiro.test(X_train.scores.clean.classic)
 X_train.scores.clean.robust <- train.robpca.2$scores[-outliers.robust,]
 shapiro.test(X_train.scores.clean.robust)
 
-plot(X_train.scores.clean.robust,col="blue")
-points(train.robpca.2$scores,pch=19)
