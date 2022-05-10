be17aee4c58ba0c53c99fe19c7cb6d573c115b0e
mitja-mandic <mitja.mandic@gmail.com>
2022-05-10 11:33:40 +0200

    done

--- project2.R
+++ project2.R
@@ -1,4 +1,14 @@
-source("libraries.R", encoding = "UTF-8")
+library(ggplot2)
+library(tidyverse)
+library(reshape2)
+library(rrcov)
+library(robustbase)
+library(corrplot)
+library(factoextra)
+library(ggfortify)
+library(pheatmap)
+library(cluster)
+
 load("Melon.rdata")
 set.seed(0875362)
 alldata <- data.frame(cbind(X,y))
@@ -10,6 +20,7 @@ colors.gr <- c("blue","red","darkgreen","orange")
 # colors for the clusters
 colors.cl <- c("cadetblue","firebrick","darkolivegreen3","gold","brown","purple")
 
+#QUESTION 1
 par(mfrow=c(2,2))
 matplot(t(mydata[mydata$y==1,]), lty=1, type="l", ylim = c(-2,4), ylab = "", main = "group 1", xlab = "Wavelength", col = colors.gr[1])
 matplot(t(mydata[mydata$y==2,]), lty=1, type="l", ylim = c(-2,4), ylab = "", main = "group 2", xlab = "Wavelength", col = colors.gr[2])
@@ -17,48 +28,57 @@ matplot(t(mydata[mydata$y==3,]), lty=1, type="l", ylim = c(-2,4), ylab = "", mai
 matplot(t(mydata[mydata$y==4,]), lty=1, type="l", ylim = c(-2,4), ylab = "", main = "group 4", xlab = "Wavelength", col = colors.gr[4])
 #mtext("Spectral plots by group", side = 3, line = -3, outer = TRUE)
 
-# x<-1:10
-# par(mar=c(2.5,2.5,1,1))
-# layout(matrix(c(1,2,3,4,1,5,3,6),ncol=2),heights=c(1,3,1,3))
-# plot.new()
-# text(0.5,0.5,"First title",cex=2,font=2)
-# plot(x)
-# plot.new()
-# text(0.5,0.5,"Second title",cex=2,font=2)
-# hist(x)
-# boxplot(x)
-# barplot(x)
-
-
-
-
-
 par(mfrow=c(1,1))
 
 pheatmap(X[order(mydata$y),], legend = TRUE, cluster_rows = F, cluster_cols = F,
          show_rownames = F, show_colnames = F,color=COL2("BrBG"))
 
+#QUESTION 2
 melon.pam2 <- pam(X, k=2)
 melon.pam3 <- pam(X, k=3)
 melon.pam4 <- pam(X, k=4)
 melon.pam5 <- pam(X, k=5)
 melon.pam6 <- pam(X, k=6)
 
-whichCluster <- melon.pam3
+whichCluster <- melon.pam2
 
 #confusion matrix
 corrplot(table(group, whichCluster$cluster), is.corr=F, method="color",
                    tl.srt=0, tl.col="black", addgrid.col="grey", addCoef.col="grey",
                    number.cex=2, cl.pos="n", col=COL1("YlGn"))
+#contingency table
 
+contingencyTable <- table(whichCluster[["clustering"]], group)
+addmargins(contingencyTable)
+
+#mosaic plot
+
+mosaicplot(t(contingencyTable), color = colors.cl, main = "", ylab = "cluster")
 
 #cluster plot
+
+#NOTE: when k=4 legends for clusters and groups merge into one (it still works ok, just instead of
+#two legends we get one)
+
 f_clust_pam <- fviz_cluster(whichCluster, geom="point", ellipse.type="convex",
                             palette=colors.cl, ellipse.border.remove = T)
 f_clust_pam[["layers"]][[1]][["data"]][["cluster"]] <- as.factor(group)
 f_clust_pam[["layers"]][[2]][["data"]][["cluster"]] <- as.factor(whichCluster$cluster)
 f_clust_pam +
   scale_colour_manual(values=colors.gr)
+
+#plots of spectra colored by clusters
+
+matplot(t(X), col = factor(whichCluster$cluster), type="l", lty=1, xlab = "Wavelength", 
+        ylab = "", main = "Spectral plot coloured according to clustering, k = 6")
+legend("topright", legend = c(unique(whichCluster[["clustering"]])), col = unique(factor(whichCluster$cluster)),
+       lty = 1, cex = 0.5, horiz = T)
+
+
+#heatmap according to clustering
+
+pheatmap(X[order(whichCluster$cluster),], legend = TRUE, cluster_rows = F, cluster_cols = F,
+         show_rownames = F, show_colnames = F,color=COL2("BrBG"))
 
 #silhouette values
 plot(silhouette(melon.pam2), col=colors.cl[1:2])
@@ -79,15 +99,9 @@ fviz_silhouette(melon.pam6, palette=colors.cl)
 #number of clusters
 fviz_nbclust(X, pam)
 
-#plots of spectra colored by clusters
 
-matplot(t(X), col = factor(whichCluster$cluster), type="l", lty=1)
 
-#heatmap according to clustering
-
-pheatmap(X[order(whichCluster$cluster),], legend = TRUE, cluster_rows = F, cluster_cols = F,
-         show_rownames = F, show_colnames = F,color=COL2("BrBG"))
-
+#QUESTION 3: hierarchical clustering
 
 melon.agnes.avg <- agnes(X)
 plot(melon.agnes.avg, which.plots=2)
@@ -100,15 +114,34 @@ plot(melon.agnes.complete.std, which.plots=2)
 
 melon.agnes.complete.dg <- as.dendrogram(melon.agnes.complete)
 plot(melon.agnes.complete.dg, leaflab="none")
+fviz_dend(melon.agnes.complete.dg, k=6, k_colors=colors.cl, show_labels = F, 
+          main = "Dendrogram of complete linkage clustering with 6 clusters")
 
-melon.agnes.complete.std.dg <- as.dendrogram(melon.agnes.complete.std)
-plot(melon.agnes.complete.std.dg, leaflab="none")
+melon.agnes.avg.dg <- as.dendrogram(melon.agnes.avg)
+fviz_dend(melon.agnes.avg, k=6, k_colors = colors.cl, show_labels = F,
+          main = "Dendrogram of average linkage clustering with 6 clusters")
 
-cutree(melon.agnes.avg, k=2)
-cutree(melon.agnes.complete, k=2)
 
-matplot(t(X), col = factor(cutree(melon.agnes.avg, k=2)), type="l", lty=1, main="Average")
-matplot(t(X), col = factor(cutree(melon.agnes.complete, k=2)), type="l", lty=1, main = "Complete", palette(colors.cl))
+agnes.avg_2 <- cutree(melon.agnes.avg, k=2)
+agnes.cmt_2 <- cutree(melon.agnes.complete, k=2)
 
+#Hierarchical clustering at k=2 and k-medoids at k=2 give the same results.
+setequal(agnes.avg_2, agnes.cmt_2)
+setequal(agnes.avg_2, melon.pam2[["clustering"]])
+
+matplot(t(X), col = factor(cutree(melon.agnes.avg, k=2)), type="l", lty=1, 
+        main="Spectral plot colored according to average linkage clustering with k = 2", ylab = "", xlab = "Wavelength")
+
+matplot(t(X), col = factor(cutree(melon.agnes.complete, k=2)), type="l", lty=1,
+        main="Spectral plot colored according to complete linkage clustering with k = 2", ylab = "", xlab = "Wavelength")
+
+matplot(t(X), col = factor(colors.cl), type="l", lty=1,
+        main="Spectral plot colored according to complete linkage clustering with k = 6", ylab = "", xlab = "Wavelength")
+
+matplot(t(X), col = factor(colors.cl), type="l", lty=1, 
+        main="Spectral plot colored according to average linkage clustering with k = 6", ylab = "", xlab = "Wavelength")
+
+
+table(cutree(melon.agnes.complete, k=6), group)
 
 
